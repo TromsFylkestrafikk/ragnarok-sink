29fd19f608b78c3981773793e8294a418f382041
Jørgen Belsaas <jorgen.belsaas@tromsfylke.no>
2024-03-15 15:04:50 +0100

    Issue #15: Cleaning up and updating test environment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,24 +10,24 @@ class TestCase extends BaseTestCase
     public function setup(): void
     {
         parent::setUp();
-        $this->withoutExceptionHandling();
-        $this->artisan('migrate', ['--database' => 'testing']);
+        // $this->withoutExceptionHandling();
+        // $this->artisan('migrate', ['--database' => 'testing']);
 
-        $this->loadMigrationsFrom(__DIR__ . '/../src/database/migrations');
-        $this->loadLaravelMigrations(['--database' => 'testing']);
+        // $this->loadMigrationsFrom(__DIR__ . '/../src/database/migrations');
+        // $this->loadLaravelMigrations(['--database' => 'testing']);
 
-        $this->withFactories(__DIR__ . '/../src/database/factories');
+        // $this->withFactories(__DIR__ . '/../src/database/factories');
     }
 
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('app.key', 'AckfSECXIvnK5r28GVIWUAxmbBSjTsmF');
-        $app['config']->set('database.default', 'testing');
-        $app['config']->set('database.connections.testing', [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-        ]);
+        // $app['config']->set('app.key', 'AckfSECXIvnK5r28GVIWUAxmbBSjTsmF');
+        // $app['config']->set('database.default', 'testing');
+        // $app['config']->set('database.connections.testing', [
+        //     'driver'   => 'sqlite',
+        //     'database' => ':memory:',
+        //     'prefix'   => '',
+        // ]);
     }
 
     protected function getPackageProviders($app)
---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Ragnarok\Sink\Tests\Unit;
+
+use Ragnarok\Sink\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BasicTest extends TestCase
+{
+    /** @test */
+    public function basicTrueTest()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function basicFalsTest()
+    {
+        $this->assertNotTrue(false);
+    }
+}
